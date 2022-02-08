c057ff9a2c11e3a067f52590a3beb32c2a1a7f5e
Adeshina <adebayo.adeshina@venturegardengroup.com>
2022-02-08 23:02:47 +0100

    Reservations API

---
+++ CinemaApi/Controllers/ReservationsController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CinemaApi.Data;
+using CinemaApi.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CinemaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+
+        private CinemaDbContext _dbContext;
+        
+        public ReservationsController(CinemaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public IActionResult Post(Reservation reservation)
+        {
+            reservation.ReservationTime = DateTime.Now;
+            _dbContext.Reservations.Add(reservation);
+            _dbContext.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+       // [Authorize(Roles = "Admin")]
+        [HttpGet("[action]")]
+        public IActionResult GetReservations()
+        {
+            // var reservations = _dbContext.Reservations;
+            // return Ok(reservations);
+            var reservations = from reservation in _dbContext.Reservations
+                               join customer in _dbContext.Users on reservation.UserId equals customer.Id
+                               join movie in _dbContext.Movies on reservation.MovieId equals movie.Id
+                               select new
+                               {
+                                   Id = reservation.Id,
+                                   ReservationTime = reservation.ReservationTime,
+                                   CustomerName = customer.Id,
+                                   MovieName = movie.Name
+                               };
+            return Ok(reservations);
+        }
+
+
+        // [Authorize(Roles = "Admin")]
+        [HttpGet("{id}")]
+        public IActionResult GetReservations(int id)
+        {
+            var r = (from reservation in _dbContext.Reservations
+                               join customer in _dbContext.Users on reservation.UserId equals customer.Id
+                               join movie in _dbContext.Movies on reservation.MovieId equals movie.Id
+                               where reservation.Id == id
+                               select new
+                               {
+                                   Id = reservation.Id,
+                                   ReservationTime = reservation.ReservationTime,
+                                   CustomerName = customer.Id,
+                                   MovieName = movie.Name,
+                                   Email = customer.Email,
+                                   Qty = reservation.Qty,
+                                   Price = reservation.Price,
+                                   Phone = reservation.Phone,
+                                   PlayingDate = movie.PlayingDate,
+                                   PlayingTime = movie.PlayingTime
+                               }).FirstOrDefault();
+            return Ok(r);
+        }
+
+        // DELETE api/<MoviesController>/5
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var reservation = _dbContext.Reservations.Find(id);
+            if (reservation == null)
+                return NotFound("No Record found of the same ID");
+            _dbContext.Reservations.Remove(reservation);
+            _dbContext.SaveChanges();
+            return Ok("Record Deleted");
+        }
+    }
+}
